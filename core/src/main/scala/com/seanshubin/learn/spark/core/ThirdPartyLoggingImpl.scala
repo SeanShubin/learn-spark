f0044a21ccc5cb6ce108cc5b4ed8616be2461f44
Sean Shubin <sshubin@cj.com>
2016-11-03 13:02:42 -0700

    Replaced JavaConversions with JavaConverters

--- core/src/main/scala/com/seanshubin/learn/spark/core/ThirdPartyLoggingImpl.scala
+++ core/src/main/scala/com/seanshubin/learn/spark/core/ThirdPartyLoggingImpl.scala
@@ -1,14 +1,18 @@
 package com.seanshubin.learn.spark.core
 
+import org.apache.log4j.{Level, LogManager, Logger}
+
+import scala.collection.JavaConverters._
+
 class ThirdPartyLoggingImpl extends ThirdPartyLogging {
   override def silenceAllLoggers(): Unit = {
-    //    val rootLogger: Logger = LogManager.getRootLogger
-    //    val untypedLoggers = JavaConversions.enumerationAsScalaIterator(LogManager.getCurrentLoggers).toSeq
-    //    val typedLoggers = untypedLoggers.map(logger => logger.asInstanceOf[Logger])
-    //    val loggers: Seq[Logger] = rootLogger +: typedLoggers
-    //    def shutLoggerOff(logger: Logger): Unit = {
-    //      logger.setLevel(Level.OFF)
-    //    }
-    //    loggers.foreach(shutLoggerOff)
+//    val rootLogger: Logger = LogManager.getRootLogger
+//    val untypedLoggers = LogManager.getCurrentLoggers.asScala.toSeq
+//    val typedLoggers = untypedLoggers.map(logger => logger.asInstanceOf[Logger])
+//    val loggers: Seq[Logger] = rootLogger +: typedLoggers
+//    def shutLoggerOff(logger: Logger): Unit = {
+//      logger.setLevel(Level.OFF)
+//    }
+//    loggers.foreach(shutLoggerOff)
   }
 }
